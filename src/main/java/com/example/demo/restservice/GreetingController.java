ee35a082e5f9eed8eda597e05b976d2a0e7ec90a
romainplt <romain.pialat@imt-atlantique.net>
2020-12-04 10:19:21 +0100

    test variable env. Marche pas pour l'instant

--- src/main/java/com/example/demo/restservice/GreetingController.java
+++ src/main/java/com/example/demo/restservice/GreetingController.java
@@ -16,13 +16,28 @@ public class GreetingController {
     private String taille;
     private String spoids;
     private String staille;
+    public Imc test_imc;
+    private String string_imc;
 
 
+    @GetMapping("/secretimc")
+    public Greeting greeting1(@RequestParam(value = "name", defaultValue = "World") String name,
+                             @RequestParam(value = "question", defaultValue = "Comment ça va ?") String question) {
+        spoids=System.getenv("POIDS");
+        staille=System.getenv("TAILLE");
+        string_imc = "poids : " + spoids + " et taille : " + staille;
+        return new Greeting(counter.incrementAndGet(), string_imc, String.format(questTemplate, question));
+    }
+
+    
     @GetMapping("/imc")
     public Imc imc(){
         poids=System.getenv("POIDS");
         taille=System.getenv("TAILLE");
-
+        System.out.print("System.getenv('POIDS') = ");
+        System.out.println(System.getenv("POIDS"));
+        System.out.print("System.getenv('TAILLE') = ");
+        System.out.println(System.getenv("TAILLE"));
         return new Imc(poids, taille);
     }
 
@@ -37,7 +52,7 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
-                             @RequestParam(value = "question", defaultValue = "Comment ça va ?") String question) {
+                             @RequestParam(value = "question", defaultValue = "Est-ceque ça marche ?") String question) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name), String.format(questTemplate, question));
     }
 }
